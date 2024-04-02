56f199fa35491fdce8e422336dcf70b09bf790ba
Aleksa <aleksa.milenovic@vegait.rs>
2024-04-02 17:34:38 +0200

    device transfer

--- batch_processing/run/batch_jobs.sh
+++ batch_processing/run/batch_jobs.sh
@@ -7,14 +7,6 @@
 #sleep 5
 #./spark/bin/spark-submit --driver-class-path postgresql-42.7.0.jar /home/batch/processing.py
 
-
-#echo "> Starting test script..."
-#sleep 5
-#./spark/bin/spark-submit /home/batch/test.py 
-
-echo "> Starting teeeest script..."
+echo "> Starting test script..."
 sleep 5
 ./spark/bin/spark-submit /home/batch/test2.py
-#./spark/bin/spark-submit --jars /geospark-1.3.1.jar /home/batch/ttteeest.py
-#./spark/bin/spark-submit --jars /geospark-1.3.1.jar,/geospark-sql-1.3.1.jar,/geospark-viz-1.3.1.jar /home/batch/ttteeest.py
-./spark/bin/spark-submit --jars /sedona-python-adapter-3.0_2.12-1.4.0.jar,/sedona-core-3.0_2.12-1.4.0.jar,/sedona-viz-3.0_2.12-1.4.0.jar /home/batch/test2.py
--- setup/cluster_up.sh
+++ setup/cluster_up.sh
@@ -4,13 +4,15 @@ echo "> Creating docker network 'big_data_network'..."
 docker network create big_data_network
 sleep 3
 
-# Starting containers using Docker Compose in the background.
+# Building custom Spark Master image.
 echo 
 cd ../batch_processing
-echo "build custom docker"
+echo "Building custom Spark Master image..."
 docker build -t custom-spark-master:latest .
 sleep 3
 
+# Starting containers using Docker Compose in the background.
+echo
 echo "> Starting containers using Docker Compose..."
 docker-compose up -d
 sleep 3
@@ -30,10 +32,6 @@ docker cp earthquake_data.csv namenode:/batch_data.csv
 docker cp tectonic_plate_boundaries.csv namenode:/tectonic_boundaries.csv
 sleep 3
 
-docker cp hospitals.csv namenode:/hospitals.csv
-docker cp countries.csv namenode:/countries.csv
-sleep 3
-
 # Wait until HDFS is out of safe mode.
 echo
 while docker exec namenode hdfs dfsadmin -safemode get | grep -q "Safe mode is ON"; do
@@ -50,17 +48,6 @@ sleep 3
 echo 
 echo "> Copying PostgreSQL JAR file to Spark Master..."
 docker cp postgresql-42.7.0.jar spark-master:./postgresql-42.7.0.jar
-sleep 3
-
-# Copying the Geospark JAR files to the Spark Master container.
-echo
-echo "> Copying Geospark JAR files to Spark Master..."
-#docker cp geospark-1.3.1.jar spark-master:./geospark-1.3.1.jar
-#docker cp geospark-sql-1.3.1.jar spark-master:./geospark-sql-1.3.1.jar
-#docker cp geospark-viz-1.3.1.jar spark-master:./geospark-viz-1.3.1.jar
-docker cp sedona-python-adapter-3.0_2.12-1.4.0.jar spark-master:./sedona-python-adapter-3.0_2.12-1.4.0.jar
-docker cp sedona-core-3.0_2.12-1.4.0.jar spark-master:./sedona-core-3.0_2.12-1.4.0.jar
-docker cp sedona-viz-3.0_2.12-1.4.0.jar spark-master:./sedona-viz-3.0_2.12-1.4.0.jar
 sleep 3
 
 # Fininshing cluster setup.
